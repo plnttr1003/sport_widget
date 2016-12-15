242d93eb651212e7b38772ad20f6312cd9d77087
plnttr1003 <top@nrdn.ru>
2016-12-15 14:07:08 +0300

    restrict

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ interact('.hall_wrapper')
     inertia: true,
     // keep the element within the area of it's parent
     restrict: {
-      restriction: "parent",
+      //restriction: "parent",
       endOnly: true,
       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
     },
